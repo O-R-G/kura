b072b40c9276f7638d294889669ca729c9617eb6
Eric Li <ericyoungli@gmail.com>
2018-06-19 17:33:15 -0400

    initial commit

--- views/home.php
+++ views/home.php
@@ -1,1 +1,12 @@
-Ciao
+<?
+$children = $oo->children(0);
+$item = $children[0];
+?>
+<div class="content" id="temp">
+  <?= $item['body']; ?>
+</div>
+
+
+<div id="logo">
+  <img src="/static/media/ck/ck.gif">
+</div>
